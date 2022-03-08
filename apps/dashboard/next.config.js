506a8b892beb60bbef178f3776e78151f048c260
Douglas Rocha <79188161+slashtp3@users.noreply.github.com>
2022-03-07 20:31:27 -0500

    fix(next.config): new /invite redirect for bot

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -21,6 +21,12 @@ module.exports = withBundleAnalyzer({
         permanent: false,
         basePath: false,
       },
+      {
+        source: '/invite',
+        destination:
+          'https://discord.com/api/oauth2/authorize?client_id=824106276404854844&permissions=8&redirect_uri=https%3A%2F%2Fwww.quanty.xyz%2F&response_type=code&scope=identify%20bot%20applications.commands%20guilds',
+        permanent: true,
+      },
     ]
   },
 })
