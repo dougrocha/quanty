afa8d6b3c0066bedb78ba720c784cd32016b10e5
Douglas Rocha <79188161+slashtp3@users.noreply.github.com>
2022-04-05 02:34:01 -0400

    feat(chore): chore

--- apps/bot/src/index.ts
+++ apps/bot/src/index.ts
@@ -5,25 +5,31 @@ const production = process.env.production
 
 dotenv.config({ path: production ? '.env.prod' : '.env' })
 
-const client = new QuantyClient(
-  {
-    token: process.env.TOKEN,
-    owner: ['571520537587875851'],
-    devGuilds: '871581301713555526',
-    defaults: true,
-    logLevel: production ? 'ALL' : 'DEBUG',
-  },
-  {
-    intents: [
-      'GUILDS',
-      'GUILD_MEMBERS',
-      'GUILD_BANS',
-      'GUILD_VOICE_STATES',
-      'GUILD_MEMBERS',
-      'GUILD_WEBHOOKS',
-    ],
-  },
-)
+class Client extends QuantyClient {
+  constructor() {
+    super(
+      {
+        token: process.env.TOKEN,
+        owner: ['571520537587875851'],
+        devGuilds: '871581301713555526',
+        defaults: true,
+        logLevel: production ? 'ALL' : 'DEBUG',
+      },
+      {
+        intents: [
+          'GUILDS',
+          'GUILD_MEMBERS',
+          'GUILD_BANS',
+          'GUILD_VOICE_STATES',
+          'GUILD_MEMBERS',
+          'GUILD_WEBHOOKS',
+        ],
+      },
+    )
+  }
+}
+
+const client = new Client()
 
 client.setDefaultCommandError(
   'This command is broken. Please contact the server owner.',
