031f9cd43a08091511834851c3f3a619d475b6d5
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-07-11 17:59:25 -0400

    changed server build script

--- apps/server/src/prisma-client-exception.filter.spec.ts
+++ apps/server/src/prisma-client-exception.filter.spec.ts
@@ -1,7 +1,7 @@
-import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter'
 
 describe('PrismaClientExceptionFilter', () => {
   it('should be defined', () => {
-    expect(new PrismaClientExceptionFilter()).toBeDefined();
-  });
-});
+    expect(new PrismaClientExceptionFilter()).toBeDefined()
+  })
+})
