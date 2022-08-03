fe0d5b5b05e44cd6fd2a44d78af6459580c95cbc
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-08-03 01:45:38 -0400

    Delete apps/dashboard/src/components/Dashboard directory

--- apps/dashboard/src/components/Dashboard/index.ts
+++
@@ -1,6 +0,0 @@
-import GuildSelectionMenu from './guildSelectionMenu'
-import DashboardNavbar from './navbar'
-import DashboardSidebar from './sidebar'
-import SidebarDrawers from './sidebarDrawer'
-
-export { GuildSelectionMenu, DashboardSidebar, DashboardNavbar, SidebarDrawers }
