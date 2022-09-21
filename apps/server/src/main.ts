ae2f70f2d9ef47c4898b321a16ccd32c3b898b66
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-09-20 20:54:21 -0400

    Set sameSite to LAX

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -39,6 +39,7 @@ export const useSessionMiddleware = session({
     httpOnly: true,
     maxAge: 60000 * 60 * 24 * 7, // 7 Days
     secure: ENV === 'production',
+    sameSite: 'lax',
     domain: ENV === 'production' ? '.quanty.xyz' : undefined,
   },
   secret: process.env.SESSION_COOKIE,
