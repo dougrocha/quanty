235eb760b24228ebf256183e0e480e189b710ba3
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-11-14 18:20:12 -0500

    Updated More HeroIcons and Mdx a-tag component

--- apps/dashboard/src/components/typography/a.tsx
+++ apps/dashboard/src/components/typography/a.tsx
@@ -1,29 +1,19 @@
 import Link from 'next/link'
-import { DetailedHTMLProps, AnchorHTMLAttributes } from 'react'
+import { AnchorHTMLAttributes } from 'react'
 
 const CustomLink = ({
   href,
   ...rest
-}: DetailedHTMLProps<
-  AnchorHTMLAttributes<HTMLAnchorElement>,
-  HTMLAnchorElement
->) => {
+}: AnchorHTMLAttributes<HTMLAnchorElement>) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
-  if (isInternalLink) {
-    return (
-      (<Link href={href} {...rest}>
-
-      </Link>)
-    );
-  }
-
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />
+  if (isInternalLink || isAnchorLink) {
+    return <Link href={href} {...rest} />
   }
 
   return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
 }
 
 export default CustomLink
+
