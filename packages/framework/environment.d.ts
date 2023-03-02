f8955e80b17ae4cace8770014c1b4c6b551267e3
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2023-03-01 18:09:18 -0700

    things

--- packages/framework/environment.d.ts
+++ packages/framework/environment.d.ts
@@ -1,16 +1,6 @@
+import { ClientOptionsSchema } from './src/util/options'
+
 declare namespace NodeJS {
-  export interface ProcessEnv {
-    BOT_TOKEN: string
-    LOG_LEVEL: 'DEBUG' | 'ALL' | 'WARN' | 'ERROR'
-
-    LAVA_LINK_HOST?: string | undefined
-    LAVA_LINK_PORT?: string | undefined
-    LAVA_LINK_PASS?: string | undefined
-
-    SPOTIFY_CLIENT_ID?: string | undefined
-    SPOTIFY_CLIENT_SECRET?: string | undefined
-
-    WS_URL?: string | undefined
-    WEBSOCKET_TOKEN?: string | undefined
-  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ProcessEnv extends ClientOptionsSchema {}
 }
