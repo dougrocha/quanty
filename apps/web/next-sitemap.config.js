8caecbd76873e8226d6e62b393cf3c26dbdd7084
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2023-03-05 00:40:48 -0700

    Updated  sitemaps

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl:
-    process.env.NEXT_PUBLIC_WEBAPP_URL ?? `https://${process.env.VERCEL_URL}`,
+  siteUrl: 'https://quanty.xyz',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
